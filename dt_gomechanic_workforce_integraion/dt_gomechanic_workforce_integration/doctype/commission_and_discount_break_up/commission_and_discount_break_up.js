e97a8608550935243280d571437e6d6f7c73f692
karshin-uppal <dev4@dipanetech.com>
2023-10-26 16:35:07 +0530

    doctype added

---
+++ dt_gomechanic_workforce_integraion/dt_gomechanic_workforce_integration/doctype/commission_and_discount_break_up/commission_and_discount_break_up.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Dipane Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Commission and Discount Break-Up', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ dt_gomechanic_workforce_integraion/dt_gomechanic_workforce_integration/doctype/commission_and_discount_break_up/test_commission_and_discount_break_up.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Dipane Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCommissionandDiscountBreakUp(FrappeTestCase):
+	pass
